c3afdee337042e06ad2409c0bb83fe359586cacc
brahmsjiang <brahmsjiang@163.com>
2020-10-26 22:51:46 +0800

    update

--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+
+import com.example.broadcastbestpractice.ui.login.LoginActivity;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +22,15 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
     }
 
     @Override
@@ -37,12 +50,20 @@ public class BaseActivity extends AppCompatActivity {
 
     class ForceOfflineReceiver extends BroadcastReceiver {
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(final Context context, Intent intent) {
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
             builder.setTitle("Warning");
             builder.setMessage("You are forced to be offline. Please try to login again.");
             builder.setCancelable(false);
-            builder.setPositiveButton("OK", new )
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
         }
     }
 }
